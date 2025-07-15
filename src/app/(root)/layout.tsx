b80efef2f7aee7779c5c17a5223250653e3d2d7f
Ajay <151801563+fjrfathurrahman@users.noreply.github.com>
2025-07-15 17:19:55 +0700

    [ fix ] : error file name

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,16 +1,7 @@
-import { Footer } from '@/components/layouts/Footer';
-import { Hero } from './_components/Hero';
-import { Summary } from './_components/Summary';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="sm:container px-2 mx-auto">
-        <Hero />
-        <Summary />
-        {children}
-        <Footer />
-      </main>
+      <main className="sm:container px-2 mx-auto">{children}</main>
     </>
   );
 }

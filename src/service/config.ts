5e0a615e79070d61352d6d98d3d05fcf7359e9cf
Ajay <151801563+fjrfathurrahman@users.noreply.github.com>
2025-07-15 17:12:08 +0700

    [ refactor ] : setting .env

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCQsYVgQVHahoJhRP0rRgVdEdfDZRenM94',
-  authDomain: 'fir-s-6efa0.firebaseapp.com',
-  databaseURL: 'https://fir-s-6efa0-default-rtdb.firebaseio.com',
-  projectId: 'fir-s-6efa0',
-  storageBucket: 'fir-s-6efa0.firebasestorage.app',
-  messagingSenderId: '19654666545',
-  appId: '1:19654666545:web:9d214835b0b17b001f6810',
-  measurementId: 'G-YL60BD2KZQ',
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
